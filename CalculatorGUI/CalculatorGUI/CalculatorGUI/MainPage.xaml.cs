a720d23f5124841ad753ba176eb318f7af3e3cee
nFocusGit <hamdevlab@gmail.com>
2016-11-22 14:30:52 +0100

    Initialized

---
+++ CalculatorGUI/CalculatorGUI/CalculatorGUI/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalculatorGUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnClick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
