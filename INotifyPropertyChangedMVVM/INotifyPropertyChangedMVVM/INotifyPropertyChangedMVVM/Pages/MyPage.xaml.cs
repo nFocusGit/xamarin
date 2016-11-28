0b10053b9aa1a64304f9d6c61d289610c6c7b367
nFocusGit <hamdevlab@gmail.com>
2016-11-28 11:09:13 +0100

    Simple Binding Sample

---
+++ INotifyPropertyChangedMVVM/INotifyPropertyChangedMVVM/INotifyPropertyChangedMVVM/Pages/MyPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using INotifyPropertyChangedMVVM.ViewModels;
+using Xamarin.Forms;
+
+namespace INotifyPropertyChangedMVVM.Pages
+{
+    public partial class MyPage : ContentPage
+    {
+        private MyViewModel myViewModel;
+
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+
+        public MyPage(MyViewModel myViewModel)
+        {
+            this.myViewModel = myViewModel;
+
+            BindingContext = this.myViewModel;
+            
+            InitializeComponent();
+
+            okButton.SetBinding(Button.IsEnabledProperty, "FornavnOk", BindingMode.OneWay);
+
+            // type safe
+            fornavnEntry.SetBinding<MyViewModel>(Entry.TextProperty, vm => vm.Fornavn, BindingMode.OneWayToSource);
+        }
+    }
+}
---
+++ INotifyPropertyChangedMVVM/INotifyPropertyChangedMVVM/INotifyPropertyChangedMVVM/ViewModels/MyViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INotifyPropertyChangedMVVM.ViewModels
+{
+    public class MyViewModel : INotifyPropertyChanged
+    {
+        private string fornavn;
+        private bool fornavnOk = false;
+        private string status = "Indtast navn";
+
+        public string Fornavn
+        {
+            get { return fornavn; }
+            set
+            {
+                if (fornavn != value)
+                {
+                    fornavn = value;
+                    OnPropertyChanged("Fornavn"); // implicit
+                                                  // or OnPropertyChanged(); explicit:  The compiler sets thes string
+
+                    if (String.IsNullOrEmpty(Fornavn) || String.IsNullOrWhiteSpace(Fornavn))
+                    {
+                        FornavnOk = false;
+                        Status = "Indtast navn...";
+                    }
+                    else
+                    {
+                        FornavnOk = true;
+                        Status = "Navn: " + Fornavn;
+                    }
+                }
+
+            }
+        }
+
+        public bool FornavnOk
+        {
+            get { return fornavnOk; }
+            set
+            {
+                if (fornavnOk != value)
+                {
+                    fornavnOk = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Status
+        {
+            get { return status; }
+            set
+            {
+                if (status != value)
+                {
+                    status = value;
+                    OnPropertyChanged();
+                }
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // CallerMemberName is used to set propertyName if none...
+        public virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var myEvent = PropertyChanged;
+            if (myEvent != null)
+            {
+                myEvent(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
