f2808bbb37a1c256276f7beb833e81b1cd56cba2
nFocusGit <hamdevlab@gmail.com>
2016-12-06 11:55:03 +0100

    WebApi Initialized

---
+++ Backend/Backend.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿using Backend.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Backend.WebApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private List<Product> products = new List<Product>();
+
+        public ProductsController()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Product p = new Product();
+                p.Id = i;
+                p.Name = "New Name: " + i;
+                Char c = Convert.ToChar(i);
+                p.Category = "Category: " + i;
+                decimal d = (decimal)i * 2;
+                p.Price = 10;
+                products.Add(p);
+            }
+
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.Find(p => p.Id == id);
+            if (product != null)
+            {
+                return Ok(product);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
