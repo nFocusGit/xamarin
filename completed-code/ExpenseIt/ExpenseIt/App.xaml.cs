a6281c337b4a411990d825bb768367c2b6ee038e
nFocusGit <hamdevlab@gmail.com>
2016-12-07 13:34:47 +0100

    Facial Recognition

---
+++ completed-code/ExpenseIt/ExpenseIt/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+// Adding Facial Recognition to Your Mobile Apps
+// https://blog.xamarin.com/adding-facial-recognition-to-your-mobile-apps/
+// https://github.com/pierceboggan/smarter-apps
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace InvoiceIt
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new InvoiceIt.InvoicesPage())
+            {
+                BarBackgroundColor = Color.FromHex("#03A9F4"),
+                BarTextColor = Color.White
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ completed-code/ExpenseIt/ExpenseIt/Expense.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Media.Abstractions;
+
+using static System.DateTime;
+using static System.String;
+
+namespace InvoiceIt
+{
+    public class Invoice
+    {
+        public double Total { get; set; } = 15.0;
+        public DateTime TimeStamp { get; set; } = UtcNow;
+        public string Photo { get; set; } = Empty;
+    }
+}
