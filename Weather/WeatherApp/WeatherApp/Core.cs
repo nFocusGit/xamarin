947f3536798b9133f77471c069da6a8fd96b548a
nFocusGit <hamdevlab@gmail.com>
2016-12-09 14:26:36 +0100

    Added: Search by City

--- Weather/WeatherApp/WeatherApp/Core.cs
+++ Weather/WeatherApp/WeatherApp/Core.cs
@@ -9,10 +9,46 @@ namespace WeatherApp
         {
             //Sign up for a free API key at http://openweathermap.org/appid
             string key = "e7de5f21f7e48a34cabd482680b17a73";
-            string queryString = "http://api.openweathermap.org/data/2.5/weather?zip="
+            string queryStringByZipCode = "http://api.openweathermap.org/data/2.5/weather?zip="
                 + zipCode + "&appid=" + key;
 
-            var results = await DataService.getDataFromService(queryString).ConfigureAwait(false);
+            string city = "London";
+            string queryStringByCity = "http://api.openweathermap.org/data/2.5/weather?q="
+    + city + "&appid=" + key;
+
+            var results = await DataService.getDataFromService(queryStringByZipCode).ConfigureAwait(false);
+
+            if (results["weather"] != null)
+            {
+                Weather weather = new Weather();
+                weather.Title = (string)results["name"];
+                weather.Temperature = (string)results["main"]["temp"] + " F";
+                weather.Wind = (string)results["wind"]["speed"] + " mph";
+                weather.Humidity = (string)results["main"]["humidity"] + " %";
+                weather.Visibility = (string)results["weather"][0]["main"];
+
+                DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+                DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
+                DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
+                weather.Sunrise = sunrise.ToString() + " UTC";
+                weather.Sunset = sunset.ToString() + " UTC";
+                return weather;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static async Task<Weather> GetWeatherByCity(string city)
+        {
+            //Sign up for a free API key at http://openweathermap.org/appid
+            string key = "e7de5f21f7e48a34cabd482680b17a73";
+
+            string queryStringByCity = "http://api.openweathermap.org/data/2.5/weather?q="
+    + city + "&appid=" + key;
+
+            var results = await DataService.getDataFromService(queryStringByCity).ConfigureAwait(false);
 
             if (results["weather"] != null)
             {
--- Weather/WeatherApp/WeatherApp/WeatherPage.xaml.cs
+++ Weather/WeatherApp/WeatherApp/WeatherPage.xaml.cs
@@ -9,13 +9,14 @@ namespace WeatherApp
         {
             InitializeComponent();
             this.Title = "Sample Weather App";
-            getWeatherBtn.Clicked += GetWeatherBtn_Clicked;
+            getWeatherByZipCodeBtn.Clicked += GetWeatherByZipCodeBtn_Clicked;
+            getWeatherByCityBtn.Clicked += GetWeatherByCityBtn_Clicked;
 
             //Set the default binding to a default object for now
             this.BindingContext = new Weather();
         }
-
-        private async void GetWeatherBtn_Clicked(object sender, EventArgs e)
+        
+        private async void GetWeatherByZipCodeBtn_Clicked(object sender, EventArgs e)
         {
             if (!String.IsNullOrEmpty(zipCodeEntry.Text))
             {
@@ -23,7 +24,20 @@ namespace WeatherApp
                 if (weather != null)
                 {
                     this.BindingContext = weather;
-                    getWeatherBtn.Text = "Search Again";
+                    getWeatherByZipCodeBtn.Text = "Search Again";
+                }
+            }
+        }
+
+        private async void GetWeatherByCityBtn_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(cityEntry.Text))
+            {
+                Weather weather = await Core.GetWeatherByCity(cityEntry.Text);
+                if (weather != null)
+                {
+                    this.BindingContext = weather;
+                    getWeatherByCityBtn.Text = "Search Again";
                 }
             }
         }
